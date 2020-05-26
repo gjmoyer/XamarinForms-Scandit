44df52c034e7a4da54fb811d637344d5fd0ae118
Greg Moyer <greg.moyer@publix.com>
2020-05-26 11:35:39 -0400

    initial code

---
+++ XamarinForms-Scandit/XamarinForms-Scandit/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Scandit.BarcodePicker.Unified;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms_Scandit
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ScanditService.ScanditLicense.AppKey = "";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Scandit.BarcodePicker.Unified.Abstractions.DidScanDelegate del = ProcessScanResults;
+            ScanService.Instance.Engage(del);
+        }
+
+        void ProcessScanResults(ScanSession session)
+        {
+            if (session == null) return;
+
+            if (session.NewlyRecognizedCodes != null && session.NewlyRecognizedCodes.Any())
+            {
+                var firstCode = session.NewlyRecognizedCodes.First();
+
+                session.StopScanning();
+
+                // Because this event handler is called from an scanner-internal thread, 
+                // you must make sure to dispatch to the main thread for any UI work.
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    ScanditService.BarcodePicker.DidScan -= ProcessScanResults;
+                    if (firstCode != null)
+                    {
+                        // This delay is needed because the app will crash if we too quickly transition to another page when the scanner is closing
+                        // This gives time for this page to load before going to item details page
+                        await Task.Delay(200);
+
+                        await ScanSearch(firstCode);
+                    }
+                });
+            }
+            else session.StopScanning();
+        }
+
+        async Task ScanSearch(Barcode scan)
+        {
+
+            await DisplayAlert("Scandit Result", $"{scan.SymbologyString} {scan.Data}", "Ok");
+        }
+    }
+}
---
+++ XamarinForms-Scandit/XamarinForms-Scandit/ScanService.cs
@@ -0,0 +1,72 @@
+﻿using Scandit.BarcodePicker.Unified;
+using Scandit.BarcodePicker.Unified.Abstractions;
+
+namespace XamarinForms_Scandit
+{
+    public class ScanService
+    {
+        static ScanService _instance;
+
+        static ScanService()
+        {
+            ScanditService.ScanditLicense.AppKey = "";
+        }
+
+        public static ScanService Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new ScanService());
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Engage(DidScanDelegate scanDelegate)
+        {
+            // Configure the barcode picker through a scan settings instance by defining which
+            // symbologies should be enabled.
+            var settings = ScanditService.BarcodePicker.GetDefaultScanSettings();
+            // prefer backward facing camera over front-facing cameras.
+            settings.CameraPositionPreference = CameraPosition.Back;
+            // Enable symbologies that you want to scan.
+            settings.EnableSymbology(Symbology.MsiPlessey, true);
+            settings.Symbologies[Symbology.MsiPlessey].Checksums = Checksum.Mod1010;
+            settings.EnableSymbology(Symbology.Interleaved2Of5, true);
+            settings.EnableSymbology(Symbology.Gs1Databar, true);
+            settings.EnableSymbology(Symbology.Gs1DatabarExpanded, true);
+            settings.EnableSymbology(Symbology.Gs1DatabarLimited, true);
+            settings.EnableSymbology(Symbology.Upca, true);
+            settings.EnableSymbology(Symbology.Upce, true);
+            settings.EnableSymbology(Symbology.Ean8, true);
+            settings.EnableSymbology(Symbology.Ean13, true);
+            settings.EnableSymbology(Symbology.Code128, true);
+
+            //reduce the area in which barcodes are detected and decoded
+            settings.RestrictedAreaScanningEnabled = true;
+            settings.ActiveScanningAreaPortrait = new Rect(0.0, 0.35, 1.0, 0.30);
+            settings.ActiveScanningAreaLandscape = new Rect(0.2, 0.35, 0.6, 0.30);
+
+            ScanditService.BarcodePicker.ScanOverlay.CameraSwitchVisibility = CameraSwitchVisibility.Never;
+
+            ScanditService.BarcodePicker.DidStop -= StopScan;
+            ScanditService.BarcodePicker.DidStop += StopScan;
+
+            ScanditService.BarcodePicker.DidScan -= scanDelegate; //in case it is already there
+            ScanditService.BarcodePicker.DidScan += scanDelegate;
+
+            ScanditService.BarcodePicker.CancelButtonText = "Cancel"; //user has option to cancel the scan
+            await ScanditService.BarcodePicker.ApplySettingsAsync(settings);
+            // Start the scanning process.
+            await ScanditService.BarcodePicker.StartScanningAsync();
+        }
+
+        void StopScan(DidStopReason reason)
+        {
+            ScanditService.BarcodePicker.DidStop -= StopScan;
+        }
+    }
+}
